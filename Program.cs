52d323bb3651767c4cd885218d44541a6659aad5
Maksym <maximsokol2288@gmail.com>
2023-02-12 19:06:55 +0200

    FCommit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson__17_Anonimous_and_Dynamic_Types_also_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
